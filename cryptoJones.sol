704e934089d66231c02fbbf5ba5bf447d17eb03c
cristian.martin <cristian.martin@agiliacenter.com>
2020-05-02 13:01:50 +0200

    WIP: Create a generate prize card function

--- cryptoJones.sol
+++ cryptoJones.sol
@@ -2,25 +2,52 @@ pragma solidity ^0.4.0;
 contract CryptoJones {
     
     uint256 public price;
-    string[9] public cards = ["skeleton", "mummy", "vampire", "skeleton", "mummy", "vampire", "skeleton", "mummy", "vampire"];
+    uint128 totalMonsterCards = 10;
+    uint128 totalPrizeCards = 20;
+    uint128 totalCards = 30;
+    int128[10] public monsterCards = [-1, -2, -3, -1, -2, -3, -1, -2, -3];
+    int128[20] prizeCards;
+    int128[30] public desk;
     mapping(address => bool) players;
     mapping(string => uint128) appearedCards;
-    string[9] public desk;
-    string selectedCard;
-    uint128 remainingCards = 9;
+    uint128 remainingCards = 30;
     
     constructor(uint256 _price) public{
         price = _price;
-        shuffle();
+    }
+    
+    function startGame() public {
+        createPrizeCards();
+        //shuffle();
+    }
+    
+    function createPrizeCards() internal {
+        uint256 totalPrize = address(this).balance;
+        for (uint256 i = 0; i < prizeCards.length; i++) {
+            if (totalPrizeCards != 1) {
+                uint256 maxPrize = totalPrize/totalPrizeCards;
+                uint256 minPrize = maxPrize/2;
+                //TODO create a getRandomNumber function
+                uint256 randomNumber = (minPrize + uint256(keccak256(abi.encodePacked(now)))) % maxPrize + minPrize;
+                prizeCards[i] = int128(randomNumber);
+                totalPrizeCards--;
+                totalPrize = totalPrize - randomNumber;
+            } else {
+                prizeCards[i] = int128(totalPrize);
+            }
+        }
     }
     
     function shuffle() internal {
-        uint256 initialLength = cards.length;
+        //TODO include the prizeCards
+        uint256 initialMonsterLength = monsterCards.length;
+        uint256 initialPrizeCardsLength = prizeCards.length;
         for (uint256 i = 0; i < desk.length; i++) {
-            uint256 randomNumber = uint256(keccak256(abi.encodePacked(now))) % initialLength;
-            desk[i] =  cards[randomNumber];
-            cards[randomNumber] = cards[initialLength - 1];
-            initialLength--;
+            //TODO create a getRandomNumber function
+            uint256 randomNumber = uint256(keccak256(abi.encodePacked(now))) % initialMonsterLength;
+            desk[i] =  monsterCards[randomNumber];
+            monsterCards[randomNumber] = monsterCards[initialMonsterLength - 1];
+            initialMonsterLength--;
         }
     }
     
@@ -34,13 +61,18 @@ contract CryptoJones {
         players[msg.sender] = true;
     }
     
+    function finishGame() private {
+        
+    }
+    
     function drawCard() public {
         uint256 randomNumber = uint256(keccak256(abi.encodePacked(now))) % (remainingCards - 1);
-        selectedCard = desk[randomNumber];
-        appearedCards[desk[randomNumber]]++;
+        /*appearedCards[desk[randomNumber]]++;
+        if (appearedCards[desk[randomNumber]] == 3) {
+            finishGame();
+        }
         desk[randomNumber] = desk[remainingCards-1]; 
-        remainingCards--;
-        //TODO comprobar si el juego ha terminado
+        remainingCards--;*/
     }
     
     function totalBalance() public view returns (uint) {
