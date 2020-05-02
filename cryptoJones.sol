756389ab2b1d08cc5453e70b2ad17f16a904bfba
Cristian Martín Redondo <c.martin.r88@gmail.com>
2020-05-02 17:11:11 +0200

    Add: Updated function shuffle including the prizes

--- cryptoJones.sol
+++ cryptoJones.sol
@@ -2,15 +2,16 @@ pragma solidity ^0.4.0;
 contract CryptoJones {
     
     uint256 public price;
-    uint128 totalMonsterCards = 10;
-    uint128 totalPrizeCards = 20;
-    uint128 totalCards = 30;
-    int128[10] public monsterCards = [-1, -2, -3, -1, -2, -3, -1, -2, -3];
-    int128[20] prizeCards;
-    int128[30] public desk;
+    uint256 totalMonsterCards = 10;
+    uint256 totalPrizeCards = 20;
+    uint256 totalCards = 30;
+    int256[10] public monsterCards = [-1, -2, -3, -1, -2, -3, -1, -2, -3];
+    int256[20] prizeCards;
+    int256[30] public desk;
     mapping(address => bool) players;
     mapping(string => uint128) appearedCards;
     uint128 remainingCards = 30;
+    bool gameStarted = false;
     
     constructor(uint256 _price) public{
         price = _price;
@@ -18,7 +19,12 @@ contract CryptoJones {
     
     function startGame() public {
         createPrizeCards();
-        //shuffle();
+        shuffle();
+    }
+    
+    function generateRandomNumber(uint256 min, uint256 max) internal view returns (uint256) {
+        //TODO generate a true getRandomNumber function
+        return(min + uint256(keccak256(abi.encodePacked(now)))) % max + min;
     }
     
     function createPrizeCards() internal {
@@ -27,8 +33,7 @@ contract CryptoJones {
             if (totalPrizeCards != 1) {
                 uint256 maxPrize = totalPrize/totalPrizeCards;
                 uint256 minPrize = maxPrize/2;
-                //TODO create a getRandomNumber function
-                uint256 randomNumber = (minPrize + uint256(keccak256(abi.encodePacked(now)))) % maxPrize + minPrize;
+                uint256 randomNumber = generateRandomNumber(minPrize, maxPrize);
                 prizeCards[i] = int128(randomNumber);
                 totalPrizeCards--;
                 totalPrize = totalPrize - randomNumber;
@@ -39,16 +44,29 @@ contract CryptoJones {
     }
     
     function shuffle() internal {
-        //TODO include the prizeCards
-        uint256 initialMonsterLength = monsterCards.length;
-        uint256 initialPrizeCardsLength = prizeCards.length;
-        for (uint256 i = 0; i < desk.length; i++) {
-            //TODO create a getRandomNumber function
-            uint256 randomNumber = uint256(keccak256(abi.encodePacked(now))) % initialMonsterLength;
-            desk[i] =  monsterCards[randomNumber];
-            monsterCards[randomNumber] = monsterCards[initialMonsterLength - 1];
-            initialMonsterLength--;
+        require(!gameStarted, "The game has already started");
+        uint256 totalCardsLength = desk.length;
+        uint256 currentPrizeCardsLength = prizeCards.length;
+        uint256 currentMonsterLength = monsterCards.length;
+        uint256 randomNumber;
+        uint128 i = 0;
+        while (i < totalCardsLength) {
+            int256 randomCard;
+            if ((generateRandomNumber(0, 1) == 1 && currentPrizeCardsLength > 0) || (currentMonsterLength == 0)) {
+                randomNumber = generateRandomNumber(0, currentPrizeCardsLength);
+                randomCard = prizeCards[randomNumber];
+                currentPrizeCardsLength--;
+                prizeCards[randomNumber] = prizeCards[currentPrizeCardsLength];
+            } else {
+                randomNumber = generateRandomNumber(0, currentMonsterLength);
+                randomCard = monsterCards[randomNumber];
+                currentMonsterLength--;
+                monsterCards[randomNumber] = monsterCards[currentMonsterLength];
+            }
+            desk[i] = randomCard;
+            i++;
         }
+        gameStarted = true;
     }
     
     function contains(address _wallet) private view returns (bool){
