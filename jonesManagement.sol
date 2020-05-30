f9675431cf9f53c2cab1bb4b5f2e051c430a8019
Cristian Martín Redondo <c.martin.r88@gmail.com>
2020-05-30 11:03:22 +0200

    New: return information about contract by index

--- jonesManagement.sol
+++ jonesManagement.sol
@@ -1,15 +1,27 @@
 pragma solidity ^0.4.0;
 import './cryptoJones.sol';
 
+
 contract JonesManagement {
-    mapping(address => uint256) public activeContracts;
+    struct cryptoJonesContract {
+        address contractAddress;
+        uint256 price;
+    }
+    uint256 public nActiveContracts = 0;
+    cryptoJonesContract[] activeContracts;
     address administrator;
     
     function createGame(uint256 _price) public returns (address){
         administrator = msg.sender;
         address newGame = new CryptoJones(_price);
-        activeContracts[newGame] = _price;
+        cryptoJonesContract memory newContract = cryptoJonesContract(newGame, _price);
+        activeContracts.push(newContract);
+        nActiveContracts++;
         return newGame;
+    }
+    
+    function getContract(uint256 index) public view returns (address contractAddress, uint256 price) {
+        return (activeContracts[index].contractAddress, activeContracts[index].price);
     }
     
 }
